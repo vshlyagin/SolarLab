382a931bf1cb4ce6d0207285ebede31a0d9e69b7
Valentine <vshlyagin@inbox.ru>
2022-07-16 00:25:18 +0300

    Initial commit

---
+++ src/main/java/com/example/SolarLab/SolarLabApplication.java
@@ -0,0 +1,13 @@
+package com.example.SolarLab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolarLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolarLabApplication.class, args);
+	}
+
+}
